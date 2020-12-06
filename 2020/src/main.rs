7ad1c65282aced42700f06d4823ddd73099592e1
Mark Junod <mark.junod@gmail.com>
2020-12-06 15:05:08 -0800

    Initial solution to Day 2, 2020

--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 mod days;
 
@@ -42,8 +43,27 @@ fn read_day_number() -> Option<u8> {
 
 fn run_day(day: u8) {
     match day {
+        0 => time_all(),
         1 => days::day1::run(),
-        _ if day == 0 || day > 25 => println!("Only days from 1 through 25 are valid"),
+        2 => days::day2::run(),
+        _ if day > 25 => println!("Only days from 1 through 25 are valid"),
         _ => println!("Day {} is not yet implemented", day),
     };
+}
+
+fn time_all() {
+    let start = current_nanos();
+
+    days::day1::run();
+    days::day2::run();
+
+    let total_time = current_nanos() - start;
+
+    let total_time_ms = (total_time as f64) / 1_000_000.0;
+
+    println!("Running all days took {} ms", total_time_ms);
+}
+
+fn current_nanos() -> u64 {
+    SystemTime::now().duration_since(UNIX_EPOCH).unwrap().subsec_nanos() as u64
 }
