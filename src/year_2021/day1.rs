483df58a8bf71eff81a0e82d393513c7fa6174e0
Mark Junod <mark.junod@gmail.com>
2021-12-16 07:52:49 -0800

    Initial solution to Day 1, 2021

--- src/year_2021/day1.rs
+++ src/year_2021/day1.rs
@@ -1,4 +1,14 @@
+use lazy_static::lazy_static;
+
 use crate::timer::time_millis;
+
+lazy_static! {
+    static ref DEPTHS: Vec<u32> = include_str!("inputs/day1.txt")
+        .trim()
+        .split("\n")
+        .map(|line| line.trim().parse::<u32>().unwrap())
+        .collect();
+}
 
 pub fn run() {
     let part1_millis = time_millis(|| run_part1());
@@ -11,9 +21,24 @@ pub fn run() {
 }
 
 fn run_part1() {
-    todo!();
+    let mut increased_count = 0;
+    for i in 1..DEPTHS.len() {
+        if DEPTHS[i] > DEPTHS[i - 1] {
+            increased_count += 1;
+        }
+    }
+
+    println!("2021 - Day 1 - Part 1: {} increasing depths", increased_count);
 }
 
 fn run_part2() {
-    todo!();
+    let mut increased_count = 0;
+    let window_depths: Vec<u32> = DEPTHS.as_slice().windows(3).map(|window| window.iter().sum()).collect();
+    for i in 1..window_depths.len() {
+        if window_depths[i] > window_depths[i - 1] {
+            increased_count += 1;
+        }
+    }
+
+    println!("2021 - Day 1 - Part 2: {} increasing windows", increased_count);
 }
