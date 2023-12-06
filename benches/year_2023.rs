52bcca66deb58247fb2ce29f2515342eab4cd088
Mark Junod <mark.junod@gmail.com>
2023-12-05 21:52:41 -0800

    add 2023 day 4 to benches

--- benches/year_2023.rs
+++ benches/year_2023.rs
@@ -7,9 +7,10 @@ fn bench_year_2023(c: &mut Criterion) {
 }
 
 fn bench_days_in_year_2023(c: &mut Criterion) {
-    c.bench_function("2022_day_1", |b| b.iter(|| year_2023::run_day(black_box(1))));
-    // c.bench_function("2022_day_2", |b| b.iter(|| year_2023::run_day(black_box(2))));
-    // c.bench_function("2022_day_3", |b| b.iter(|| year_2023::run_day(black_box(3))));
+    c.bench_function("2023_day_1", |b| b.iter(|| year_2023::run_day(black_box(1))));
+    c.bench_function("2023_day_2", |b| b.iter(|| year_2023::run_day(black_box(2))));
+    c.bench_function("2023_day_3", |b| b.iter(|| year_2023::run_day(black_box(3))));
+    c.bench_function("2023_day_4", |b| b.iter(|| year_2023::run_day(black_box(4))));
 }
 
 criterion_group!(run_entire_year, bench_year_2023);
