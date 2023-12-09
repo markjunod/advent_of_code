49444b4611c712330996da941d4b79d0df525a75
Mark Junod <mark.junod@gmail.com>
2023-12-08 20:17:58 -0800

    Improved Day 5 2023 solution using ranges

--- src/year_2023/day5.rs
+++ src/year_2023/day5.rs
@@ -10,7 +10,7 @@ lazy_static! {
 #[derive(Debug, Clone, Default)]
 struct Almanac {
     seeds: Vec<i64>,
-    seed_ranges: Vec<SeedRange>,
+    seed_ranges: Vec<Range>,
     seed_to_soil: Vec<AlmanacMapRange>,
     soil_to_fertilizer: Vec<AlmanacMapRange>,
     fertilizer_to_water: Vec<AlmanacMapRange>,
@@ -31,7 +31,7 @@ impl From<&str> for Almanac {
                 match current_map {
                     "seeds" => {
                         almanac.seeds = line.trim().split_ascii_whitespace().map(|n| n.parse::<i64>().unwrap()).collect();
-                        almanac.seeds.clone().chunks(2).for_each(|chunk| almanac.seed_ranges.push(SeedRange { min: chunk[0], length: chunk[1] }));
+                        almanac.seeds.clone().chunks(2).for_each(|chunk| almanac.seed_ranges.push(Range { min: chunk[0], length: chunk[1] }));
                     },
                     "seed-" => almanac.seed_to_soil.push(AlmanacMapRange::from(line.trim())),
                     "soil-" => almanac.soil_to_fertilizer.push(AlmanacMapRange::from(line.trim())),
@@ -50,7 +50,7 @@ impl From<&str> for Almanac {
 }
 
 #[derive(Debug, Clone, Default)]
-struct SeedRange {
+struct Range {
     min: i64,
     length: i64,
 }
@@ -91,16 +91,6 @@ fn run_part1() {
     info!("2023 - Day 5 - Part 1: Lowest location number {}", min_local);
 }
 
-fn run_part2() {
-    let locals = ALMANAC.seed_ranges.iter().flat_map(|seed_range| {
-        (seed_range.min..(seed_range.min + seed_range.length)).map(get_seed_local)
-    });
-
-    let min_local = locals.min().unwrap();
-
-    info!("2023 - Day 5 - Part 2: Lowest location number {}", min_local);
-}
-
 fn get_seed_local(seed: i64) -> i64 {
     let soil = get_destination(seed, &ALMANAC.seed_to_soil);
     let fertilizer = get_destination(soil, &ALMANAC.soil_to_fertilizer);
@@ -119,4 +109,54 @@ fn get_destination(source: i64, ranges: &Vec<AlmanacMapRange>) -> i64 {
     }
 
     source
+}
+
+fn run_part2() {
+    let locals = ALMANAC.seed_ranges.iter().flat_map(|seed_range| {
+        let mut seed = seed_range.min;
+        let mut locals = Vec::<i64>::new();
+        let mut count = 0;
+        while count < 5 && seed < seed_range.min + seed_range.length {
+            let increasing_range = get_seed_local_with_range(seed);
+            locals.push(increasing_range.min);
+            seed += increasing_range.length;
+            count += 1;
+        }
+
+        locals
+    });
+
+    let min_local = locals.min().unwrap();
+
+    info!("2023 - Day 5 - Part 2: Lowest location number {}", min_local);
+}
+
+fn get_seed_local_with_range(seed: i64) -> Range {
+    let soil_range = get_destination_with_range(seed, &ALMANAC.seed_to_soil);
+    let fertilizer_range = get_destination_with_range(soil_range.min, &ALMANAC.soil_to_fertilizer);
+    let water_range = get_destination_with_range(fertilizer_range.min, &ALMANAC.fertilizer_to_water);
+    let light_range = get_destination_with_range(water_range.min, &ALMANAC.water_to_light);
+    let temp_range = get_destination_with_range(light_range.min, &ALMANAC.light_to_temp);
+    let humid_range = get_destination_with_range(temp_range.min, &ALMANAC.temp_to_humidity);
+    let local_range = get_destination_with_range(humid_range.min, &ALMANAC.humidity_to_local);
+
+    Range {
+        min: local_range.min,
+        length: vec![soil_range.length, fertilizer_range.length, water_range.length, light_range.length, temp_range.length, humid_range.length, local_range.length].into_iter().min().unwrap(),
+    }
+}
+
+fn get_destination_with_range(source: i64, ranges: &Vec<AlmanacMapRange>) -> Range {
+    let mut min_next_range = i64::MAX;
+    for range in ranges {
+        if source >= range.source_min && source < range.source_min + range.length {
+            let min = source + (range.destination_min - range.source_min);
+            let length = range.destination_min + range.length - min;
+            return Range { min, length};
+        } else if source < range.source_min && range.source_min < min_next_range {
+            min_next_range = range.source_min;
+        }
+    }
+
+    Range { min: source, length: min_next_range - source }
 }
